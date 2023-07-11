81610c475b37a34023a381befcef1f0b7edc8b2e
John Boyle <nikolasboyle@gmail.com>
2023-07-10 20:11:56 -0700

    implement recipe create, update and delete

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -15,21 +15,68 @@ async function getRecipe(req, res) {
 
 async function createRecipe(req, res) {
   const recipe = await models.Recipe.create(req.body);
-  res.send(recipe);
+
+  // Strip client ID's and assign recipe ID
+  const ingredients = req.body.ingredients.map(({ id, ...ingredient }) => ({
+    ...ingredient,
+    recipeId: recipe.id,
+  }));
+  const instructions = req.body.instructions.map(({ id, ...instruction }) => ({
+    ...instruction,
+    recipeId: recipe.id,
+  }));
+
+  // Create new ingredients and instructions
+  await models.Ingredient.bulkCreate(ingredients);
+  await models.Instruction.bulkCreate(instructions);
+
+  res.send({ id: recipe.id });
 }
 
 async function updateRecipe(req, res) {
   const { recipeId } = req.params;
+
   const recipe = models.Recipe.findByPk(recipeId);
-  await recipe.update(req.body);
-  res.send(recipe);
+
+  if (!recipe) {
+    throw new Error("Recipe not found");
+  }
+
+  await models.Recipe.update(req.body, { where: { id: recipeId } });
+
+  // Delete old ingredients and instructions
+  await models.Ingredient.destroy({ where: { recipeId } });
+  await models.Instruction.destroy({ where: { recipeId } });
+
+  // Strip client ID's and assign recipe ID
+  const ingredients = req.body.ingredients.map(({ id, ...ingredient }) => ({
+    ...ingredient,
+    recipeId,
+  }));
+  const instructions = req.body.instructions.map(({ id, ...instruction }) => ({
+    ...instruction,
+    recipeId,
+  }));
+
+  // Create new ingredients and instructions
+  await models.Ingredient.bulkCreate(ingredients);
+  await models.Instruction.bulkCreate(instructions);
+
+  res.send({ id: recipeId });
 }
 
 async function deleteRecipe(req, res) {
   const { recipeId } = req.params;
-  const recipe = models.Recipe.findByPk(recipeId);
+
+  const recipe = await models.Recipe.findByPk(recipeId);
+
+  if (!recipe) {
+    throw new Error("Recipe not found");
+  }
+
   await recipe.destroy();
-  res.send(recipeId);
+
+  res.send({ id: recipeId });
 }
 
 export default {
